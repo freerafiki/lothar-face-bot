2b01dd68b05216bdcc57432151ac31dd66a26ea1
enricofacca <enrico.facca@sns.it>
2022-02-01 10:00:18 +0100

    more pressure

--- lothar-face-bot.py
+++ lothar-face-bot.py
@@ -255,7 +255,7 @@ def classify_photo(update: Update, context: CallbackContext) -> None:
                             lothars_f += " e " + lothars_found[i]
                     update.message.reply_text(f"trovati {lothars_f} nella foto!")
             else:
-                update.message.reply_text("trovato una faccia, ma non lothar")
+                update.message.reply_text("trovato una faccia, ma non lothar. Magari c'è, però non ce l'ho fatta. Se solo avessi più selfies tra cui cercare...")
         else:
             update.message.reply_text("non sembrano esserci facce, o non avete ancora inviato le foto a Facca.")
     else:
